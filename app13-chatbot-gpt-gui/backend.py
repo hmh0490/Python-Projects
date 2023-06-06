e35933bff6b6d37e20ffb943d186f3e26a093049
hmh0490 <116732067+hmh0490@users.noreply.github.com>
2023-06-06 21:41:26 +0200

    upload app13

---
+++ app13-chatbot-gpt-gui/backend.py
@@ -0,0 +1,23 @@
+import openai
+
+
+class Chatbot:
+    def __init__(self):
+        openai.api_key = "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"
+
+    def get_response(self, user_input):
+        # tokens = max words the bot should provide
+        # temperature = 0 : more accurate answers but less diverse
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=user_input,
+            max_tokens=3000,
+            temperature=0.5
+        ).choices[0].text
+        return response
+
+
+if __name__ == "__main__":
+    chatbot = Chatbot()
+    response = chatbot.get_response("Write a joke about birds.")
+    print(response)
