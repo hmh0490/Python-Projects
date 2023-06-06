d998f8feda518d5beaf7023dcde1ba03d977f141
hmh0490 <116732067+hmh0490@users.noreply.github.com>
2023-06-06 21:30:52 +0200

    upload app10

---
+++ main.py
@@ -0,0 +1,79 @@
+import requests
+# extract only necessary info based on selected tag
+import selectorlib
+import smtplib, ssl
+import time
+import sqlite3
+
+URL = "https://programmer100.pythonanywhere.com/tours/"
+# tells the webserver which hosts this webpage, in this case it tells which
+# web browser is used, if you URL is not enough, having a header may fixes the
+# issue
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+# Establish a connection
+connection = sqlite3.connect("data.db")
+
+def scrape(url):
+    """Scrape the page source from the URL"""
+    response = requests.get(url, headers=HEADERS)
+    source = response.text
+    return source
+
+
+def extract(source):
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value = extractor.extract(source)["tours"]
+    return value
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "hajni0490@gmail.com"
+    password = "bfmgebgukmcdgtyr"
+
+    receiver = "hajni0490@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+    print("Email was sent!")
+
+
+def store(extracted):
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+    # write changes to the database
+    connection.commit()
+
+def read(extracted):
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    band, city, date = row
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?",
+                   (band, city, date))
+    # fetchall + execute returns a list, fetchall + executemany returns a tuple
+    rows = cursor.fetchall()
+    print(rows)
+    return rows
+
+if __name__ == "__main__":
+    while True:
+        scraped = scrape(URL)
+        extracted = extract(scraped)
+        print(extracted)
+
+        if extracted != "No upcoming tours":
+            row = read(extracted)
+            # if there are no such rows in database
+            if not row:
+                store(extracted)
+                send_email(message="Hey, new event was found!")
+        time.sleep(2)
